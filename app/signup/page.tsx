b618ff5a9fb3aeba98a678b94c80590a3a9c0c38
SakicHarun <75125150+SakicHarun@users.noreply.github.com>
2024-07-31 12:43:50 +0200

    Create page.tsx

---
+++ app/signup/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Registration from '../components/Registration'
+
+const page = () => {
+  return (
+    <>  
+    <Registration/>
+    
+    </>
+  )
+}
+
+export default page
