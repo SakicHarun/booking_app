9fc3a5ed5ca55b9768a5c647471dc11018c03760
SakicHarun <75125150+SakicHarun@users.noreply.github.com>
2024-07-31 12:43:29 +0200

    Create page.tsx

---
+++ app/selectedaccomodation/page.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import SelectedAccomoadation from '../components/SelectedAccomoadation'
+
+const page = () => {
+  return (
+   <>   
+   <Navbar/>
+    <SelectedAccomoadation/>
+   <Footer/>
+   
+   </>
+  )
+}
+
+export default page
