7aac6e27f5f150fec293545b46107e7772a88876
Sakic Harun <harun.sakic2019@size.ba>
2024-08-01 17:25:00 +0200

    Zavrsena stranica selected accomondation i poceta reservationdetails.tsx

---
+++ app/reservationdetails/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import ReservationDetails from '../components/ReservationDetails'
+import Footer from '../components/Footer'
+
+const page = () => {
+  return (
+    <> 
+    <Navbar/>
+    <ReservationDetails/>
+    <Footer/>
+    </>
+  )
+}
+
+export default page
