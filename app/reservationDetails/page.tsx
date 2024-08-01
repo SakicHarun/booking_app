946cd0ee9211f092f7fb4ef4058964c11c10f97f
Sadžida Valjevac <valjevac.sadzida02@gmail.com>
2024-08-01 18:50:24 +0200

    Pravljenje Reservation Details

--- app/reservationDetails/page.tsx
+++ app/reservationDetails/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
+import ReservationDetails from '../components/ReservationDetails'
 function page() {
   return (
     <div>
-        <p>helloooooooooo</p>
+        <Navbar></Navbar>
+        <ReservationDetails></ReservationDetails>
+        <Footer></Footer>
       
     </div>
   )
