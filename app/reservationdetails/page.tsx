a86f29a2e3581e2bf7781c66800f35202c468970
Sakic Harun <harun.sakic2019@size.ba>
2024-08-02 15:29:28 +0200

    azuriranje

---
+++ app/reservationdetails/page.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import Navbar from '../components/Navbar'
+import ReservationDetails from '../components/ReservationDetails'
+import Footer from '../components/Footer'
+
+const page = () => {
+  return (
+   <>
+   <Navbar/>
+   <ReservationDetails/>
+   <Footer/>
+   </>
+  )
+}
+
+export default page
